ff12f35a844562311696b429439407208ce59759
sahil <sahilpanchal2903@gmail.com>
2023-11-03 17:08:52 +0000

    Added pillars

--- src/components/qualities/Qualites.tsx
+++ src/components/qualities/Qualites.tsx
@@ -1,20 +1,49 @@
-import { Stack } from "@mui/material";
+import { Box, Divider, Stack, Typography } from "@mui/material";
 import Quotes from "./Quotes";
 
+
+const quo = [
+  {
+    logo: "pound",
+    text: "100% Donation Transparency: Every Penny you give goes directly to those in need.",
+  },
+  {
+    logo: "hand",
+    text: "Impactful Change: Your support creates tangible, life-changing results",
+  },
+  {
+    logo: "heart",
+    text: "Community of Compassion: Join a network dedicated to making the world a better place",
+  },
+];
 function Qualites() {
   return (
     <>
+      <Divider />
       <Stack
-        spacing={3}
         sx={{
+          marginY: 5,
           display: "flex",
           justifyContent: "center",
           alignItems: "center",
         }}
       >
-        <Quotes />
-        <Quotes />
-        <Quotes />
+        <Typography fontFamily={'var(--font-fam)'} variant="h4" sx={{
+          textAlign: 'center',
+          marginBottom: 5,
+        }}>
+          Our Three Pillars of Impact
+        </Typography>
+        <Stack
+          spacing={2}
+          sx={{
+            alignItems: "start",
+          }}
+        >
+          {quo.map((x) => (
+            <Quotes logo={x.logo} text={x.text} />
+          ))}
+        </Stack>
       </Stack>
     </>
   );
--- src/components/qualities/Quotes.tsx
+++ src/components/qualities/Quotes.tsx
@@ -1,49 +1,72 @@
-import { Box, Icon, IconButton, Paper, Stack, Typography } from "@mui/material";
+import {
+  Grid,
+  Typography,
+} from "@mui/material";
 
-// import CurrencyPoundIcon from "@mui/icons-material/CurrencyPound";
-// import FavoriteIcon from "@mui/icons-material/Favorite";
-// import HandshakeIcon from "@mui/icons-material/Handshake";
-import CurrencyPound from "@mui/icons-material/CurrencyPound";
+import CurrencyPoundIcon from "@mui/icons-material/CurrencyPound";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import HandshakeIcon from "@mui/icons-material/Handshake";
 
-// interface QuotesProp {
-//   text?: string;
-//   logo?: string;
-// }
+interface QuotesProp {
+  text?: string;
+  logo?: string;
+}
 
-function Quotes() {
-  return (
-    <>
-      <Stack direction={"row"} spacing={"1rem"}>
-        <Paper
+function Quotes(p: QuotesProp) {
+  const logo = () => {
+    if (p.logo === "pound") {
+      return (
+        <CurrencyPoundIcon
           style={{
-            width: 80, // Set the desired width and height for the circle
-            height: 80,
-            borderRadius: "50%", // Make it circular by setting the border-radius to 50%
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
-            backgroundColor: 'var(--gray)'
-          }}
-        >
-          <CurrencyPound style={{
             width: 40,
             height: 40,
-          }}/>
-        </Paper>
-        <Box
-          style={{
-            backgroundColor: 'var(--gray)',
-            borderLeft: '5px solid var(--blue)',
-            padding: 10
           }}
+        />
+      );
+    } else if (p.logo === "hand") {
+      return (
+        <HandshakeIcon
+          style={{
+            width: 40,
+            height: 40,
+          }}
+        />
+      );
+    } else {
+      return (
+        <FavoriteIcon
+          style={{
+            width: 40,
+            height: 40,
+          }}
+        />
+      );
+    }
+  };
+  return (
+    <>
+      <Grid container>
+        <Grid
+          item
+          style={{
+            backgroundColor: "var(--gray)",
+            borderLeft: "5px solid var(--blue)",
+            padding: 11,
+          }}
+          xs
         >
-          <Typography 
+          <Typography
             fontFamily={"var(--font-fam)"}
+            fontWeight={"bold"}
+            fontSize={'1rem'}
+            sx={{
+              padding: 2,
+            }}
           >
-            Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit enim labore culpa sint ad nisi
+            {p.text}
           </Typography>
-        </Box>
-      </Stack>
+        </Grid>
+      </Grid>
     </>
   );
 }
