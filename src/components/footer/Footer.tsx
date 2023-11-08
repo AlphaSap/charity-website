fe0820d07ddc346b6b7d9f04fbf25803d126c79a
sahil <sahilpanchal2903@gmail.com>
2023-11-08 19:05:25 +0000

    Final Change to footer for today

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,6 +1,6 @@
 import { Box, Container, Grid, Link, Typography } from "@mui/material";
 import "./Footer.css";
-import { Facebook, Instagram, Twitter } from "@mui/icons-material";
+import { Facebook, GitHub, Instagram} from "@mui/icons-material";
 
 function Footer() {
   // return (
@@ -57,19 +57,10 @@ function Footer() {
         <Grid container spacing={5}>
           <Grid item xs={12} sm={4}>
             <Typography variant="h6" color="text.primary" gutterBottom>
-              About Us
-            </Typography>
-            <Typography variant="body2" color="text.secondary">
-              We are XYZ company, dedicated to providing the best service to our
-              customers.
-            </Typography>
-          </Grid>
-          <Grid item xs={12} sm={4}>
-            <Typography variant="h6" color="text.primary" gutterBottom>
               Contact Us
             </Typography>
             <Typography variant="body2" color="text.secondary">
-              123 Main Street, Anytown, USA
+              17 Onra Road, London E17 8JD
             </Typography>
             <Typography variant="body2" color="text.secondary">
               Email: info@example.com
@@ -80,7 +71,7 @@ function Footer() {
           </Grid>
           <Grid item xs={12} sm={4}>
             <Typography variant="h6" color="text.primary" gutterBottom>
-              Follow Us
+              Socials
             </Typography>
             <Link href="https://www.facebook.com/" color="inherit">
               <Facebook />
@@ -92,18 +83,35 @@ function Footer() {
             >
               <Instagram />
             </Link>
-            <Link href="https://www.twitter.com/" color="inherit">
-              <Twitter />
+            <Link
+              href="https://github.com/AlphaSap/charity-website"
+              color="inherit"
+            >
+              <GitHub />
+            </Link>
+          </Grid>
+          <Grid item xs={12} sm={4}>
+            <Typography variant="h6" color="text.primary" gutterBottom>
+              Repository
+            </Typography>
+
+            <Link
+              href="https://github.com/AlphaSap/charity-website"
+              color="inherit"
+            >
+              <GitHub />
             </Link>
           </Grid>
         </Grid>
         <Box mt={5}>
           <Typography variant="body2" color="text.secondary" align="center">
             {"Copyright © "}
-            <Link color="inherit" href="https://your-website.com/">
-              Your Website
+            <Link color="inherit" href="#">
+              Dholar Community
             </Link>{" "}
             {new Date().getFullYear()}
+            <br />
+            Charity number: 1203557
             {"."}
           </Typography>
         </Box>
