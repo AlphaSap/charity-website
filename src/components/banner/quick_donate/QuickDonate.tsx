25541ed3a3bada7fde051edf4915cd4e3ce7b464
Sahil <sahilpanchal2903@gmail.com>
2024-01-01 18:23:52 +0000

    cusotm amount

--- src/components/banner/quick_donate/QuickDonate.tsx
+++ src/components/banner/quick_donate/QuickDonate.tsx
@@ -9,8 +9,10 @@ import Box from "@mui/material/Box";
 import {
   Backdrop,
   CircularProgress,
+  Grid,
   Modal,
   Stack,
+  TextField,
   ThemeProvider,
   Typography,
   createTheme,
@@ -43,7 +45,9 @@ const style = {
 
 function QuickDonate() {
   const [amount, setAmount] = useState("");
+  const [customAmount, setCustomAmount] = useState(0);
   const [open, setOpen] = useState(false);
+  const [openCustom, setOpenCustom] = useState(false);
 
   const [loading, setLoading] = useState(false);
   const handleClose = () => {
@@ -88,6 +92,8 @@ function QuickDonate() {
 
     if (amount === "custom") {
       console.log("We got custom");
+      setAmount("");
+      setOpenCustom(true);
     } else {
       const a = Number.parseInt(amount);
       if (!a) {
@@ -96,6 +102,48 @@ function QuickDonate() {
       // we have a good number and can carry on now
       setOpen(true);
     }
+  };
+
+  function isNumeric(str: any) {
+    if (typeof str != "string") return false; // we only process strings!
+    return (
+      !isNaN(str) && // use type coercion to parse the _entirety_ of the string (`parseFloat` alone does not do this)...
+      !isNaN(parseFloat(str))
+    );
+  }
+
+  const handleCustomCheckOut = () => {
+    console.log("lcajkh " + customAmount);
+    if (customAmount === 0) {
+      setOpenCustom(false);
+      return;
+    }
+
+    fetch("https://charity-web-server.vercel.app/checkout", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        amount: customAmount,
+      }),
+    })
+      .then((res) => {
+        if (res.ok) {
+          return res.json();
+        }
+        return res.json().then((json) => Promise.reject(json));
+      })
+      .then(({ url }) => {
+        setLoading(false);
+        window.location = url;
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+
+    setLoading(true);
+    setOpenCustom(false);
   };
 
   const greenButton = {
@@ -121,17 +169,68 @@ function QuickDonate() {
         marginTop={2}
         sx={{ display: "flex", justifyContent: "center", alignItems: "center" }}
       >
-        <Button variant="contained" onClick={handleCheckOut} sx={greenButton}>
+        <Button
+          variant="contained"
+          onClick={handleCustomCheckOut}
+          sx={greenButton}
+        >
           To Checkout
         </Button>
         <Button
           variant="text"
-          onClick={handleClose}
+          onClick={() => setOpen(false)}
           sx={{ color: "black", marginX: 2 }}
         >
           Close
         </Button>
       </Box>
+    </>
+  );
+
+  const custom = (
+    <>
+      <Typography id="modal-modal-title" variant="h6" component="h2">
+        Enter the amount you wish to donate!
+      </Typography>
+      <Typography id="modal-modal-title" variant="body1" component="h2">
+        Let's go to checkout and complete the Donation!!
+      </Typography>
+
+      <Grid container gap={1} sx={{ mt: 2 }}>
+        <Grid item xs={12}>
+          <TextField
+            id="outlined-basic"
+            label="Enter Amount"
+            variant="outlined"
+            value={customAmount}
+            onChange={(e) => {
+              const am = Number.parseInt(e.target.value);
+              if (!am) {
+                return;
+              }
+              setCustomAmount(am);
+            }}
+          />
+        </Grid>
+        <Grid item xs={5}>
+          <Button
+            variant="contained"
+            onClick={handleCustomCheckOut}
+            sx={greenButton}
+          >
+            To Checkout
+          </Button>
+        </Grid>
+        <Grid item xs={5}>
+          <Button
+            variant="text"
+            onClick={() => setOpenCustom(false)}
+            sx={{ color: "black", marginX: 2 }}
+          >
+            Close
+          </Button>
+        </Grid>
+      </Grid>
     </>
   );
 
@@ -152,6 +251,17 @@ function QuickDonate() {
         }}
       >
         <Box sx={style}>{standardAmountModal}</Box>
+      </Modal>
+
+      <Modal
+        open={openCustom}
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Box sx={style}>{custom}</Box>
       </Modal>
       <Stack
         spacing={3}
@@ -201,6 +311,9 @@ function QuickDonate() {
               </MenuItem>
               <MenuItem value={"100"} id="5">
                 £100
+              </MenuItem>
+              <MenuItem value={"custom"} id="6">
+                Custom
               </MenuItem>
             </Select>
           </FormControl>
